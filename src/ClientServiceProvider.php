ba121c5994aca9da5ede1cc67cc49ef86608d6e0
CrazyCodes <c919342864@yahoo.com>
2018-01-31 14:55:08 +0800

    add client function

---
+++ src/ClientServiceProvider.php
@@ -0,0 +1,7 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: crazy
+	 * Date: 2018/1/31
+	 * Time: 14:51
+	 */
---
+++ src/Helpers/Function.php
@@ -0,0 +1,22 @@
+<?php
+	function T($service = '', $action = '', $data = [])
+	{
+		$client = new \swoole_client(SWOOLE_SOCK_TCP);
+		
+		if (!$client->connect (env ("CLIENT_SERVER_URL"), env ("CLIENT_SERVER_PORT"), -1)) {
+			exit("connect failed. Error: {$this->client->errCode}\n");
+		}
+		$data = json_encode ([
+			'service' => $service,
+			'action'  => $action,
+			'data'    => $data,
+		]);
+		
+		$client->send ($data);
+		
+		$result = $client->recv ();
+		
+		$client->close ();
+		
+		return $result;
+	}
